d4dc26d18cd2ee6daf026558d1188b19e5848dc4
Vpsush <vpsush2023@gmail.com>
2023-11-02 22:45:38 +0200

    1

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,5 @@
 // import css from './Button.module.css';
 
-// const Button = ({ onClick }) => {
-//   return (
-//     <button type="button" className={css.Button} onClick={onClick}>
-//       Load more
-//     </button>
-//   );
-// };
-
-// export default Button;
-
 import css from './Button.module.css';
 
 const Button = ({ onClick }) => {
